2f8183c1c2f64a6d5e8ac34b7f8bd63a71533258
Anders Rundgren <anders.rundgren.net@gmail.com>
2019-11-03 18:10:45 +0100

    Update LocalIntegrationService.java

--- src/org/webpki/webapps/swedbank_psd2_saturn/LocalIntegrationService.java
+++ src/org/webpki/webapps/swedbank_psd2_saturn/LocalIntegrationService.java
@@ -295,7 +295,7 @@ public class LocalIntegrationService extends InitPropertyReader implements Servl
             svgCardImage = getEmbeddedResourceString(SVG_CARD_IMAGE);
 
             ////////////////////////////////////////////////////////////////////////////////////////////
-            // Create a CA keys.  Note Saturn payment credentials do not use PKI
+            // Create a CA keys.  Payment credentials only use keys but KeyGen2 wraps keys in PKI
             ////////////////////////////////////////////////////////////////////////////////////////////
             KeyPairGenerator generator = KeyPairGenerator.getInstance("EC");
             generator.initialize(new ECGenParameterSpec(KeyAlgorithms.NIST_P_256.getJceName()),
